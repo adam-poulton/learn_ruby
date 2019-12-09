279e60791438af48f41358132b2363f469bd64a1
adam-poulton <adam.poulton5@gmail.com>
2019-12-09 21:03:26 +1100

    Complete calculator.rb

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,38 @@
 #write your code here
+def multiply(a, b)
+  a * b
+end
+
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def sum(nums)
+  sum = 0
+  while nums.length > 0
+    sum += nums.pop()
+  end
+  sum
+end
+
+def power(a,b)
+  sum = 1
+  while b > 0
+    sum *= a
+    b -= 1
+  end
+  sum
+end
+
+def factorial(n)
+  sum = 1
+  while n > 0
+    sum *= n
+    n -= 1
+  end
+  sum
+end
