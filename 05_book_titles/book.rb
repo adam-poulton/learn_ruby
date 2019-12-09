6af73970b785e18d0e0d7726b67d170fb196920f
adam-poulton <adam.poulton5@gmail.com>
2019-12-09 23:17:21 +1100

    Complete book_titles.rb

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,16 @@
 class Book
 # write your code here
+  attr_reader :title
+
+  def title=(title)
+    littles = %w{a an and the in of}
+    words = title.split.map do |word|
+      if littles.include?(word)
+        word
+      else
+        word.capitalize
+      end
+    end
+    @title = ([words[0].capitalize] + words[1..-1]).join(" ")
+  end
 end
