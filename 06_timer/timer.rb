3d113089807cd3f8d6585dfe264387f7cd3c569b
adam-poulton <adam.poulton5@gmail.com>
2019-12-09 23:44:22 +1100

    Complete timer.rb

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,24 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def initialize(seconds=0)
+    @seconds = seconds
+  end
+
+  def time_string
+    hours = padded(@seconds / 3600)
+    mins = padded((@seconds % 3600) / 60)
+    secs = padded((@seconds % 3600) % 60)    
+    "#{hours}:#{mins}:#{secs}"
+  end
+
+  def padded(num)
+    if num < 10
+      '0' + num.to_s
+    else
+      num.to_s
+    end
+  end
+
 end
